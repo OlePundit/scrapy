49bf7f4ae83fb7ea8cb4b1709917a15744421397
OlePundit <glennomondi@yahoo.com>
2024-11-01 05:12:06 +0300

    removing replace

--- sansacrawling/sansacrawling/spiders/crawling_spider.py
+++ sansacrawling/sansacrawling/spiders/crawling_spider.py
@@ -3,12 +3,23 @@ from scrapy.linkextractors import LinkExtractor
 
 class CrawlingSpider(CrawlSpider):
     name = "mycrawler"
-    allowed_domains = ["twitter.com"]
-    start_urls = ["https://thejobadverts.com/jobs/"]
+    allowed_domains = ["thejobadverts.com"]
+    start_urls = [
+        "https://thejobadverts.com/jobs/",
+        "https://thejobadverts.com/jobs/it-projects-assistant/"
+    ]
 
 
     rules = (
-        Rule(LinkExtractor(allow="hr_stellar"), callback="parse_item"),
+        Rule(LinkExtractor(allow=r"/jobs/"), callback="parse_item"),
     )
 
     def parse_item(self, response):
+        yield{
+            "title": response.css("h1::text").get(),
+            "location_category_price": response.css("header ul li::text").getall(),
+            "deadline": response.css(".entry-content .entry-meta li::text")[1].get(),
+            "description": " ".join(
+                response.css(".entry-content *::text").getall()
+            ).strip()        
+        }
